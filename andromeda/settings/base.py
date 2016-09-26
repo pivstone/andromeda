0c6dcb8b582269f78de66e22ed35b1be971f97c2
王小磊 <pivstone@gmail.com>
2016-09-26 11:41:42 +0800

    🐍 add six for  configparser

--- andromeda/settings/base.py
+++ andromeda/settings/base.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import configparser
+from django.utils.six.moves import configparser
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
